0ac9b101281c6b4f4e1562bfbcc950a5cb643f4b
nathalybohorquez16-glitch <nathalybohorquez16@gmail.com>
2026-07-17 02:56:59 -0500

    Implement Restaurante class with product and client management

--- servicios/restaurante.py
+++ servicios/restaurante.py
@@ -1,1 +1,41 @@
+from modelos.producto import Producto
+from modelos.cliente import Cliente
 
+
+class Restaurante:
+
+    def __init__(self):
+        self.productos: list[Producto] = []
+        self.clientes: list[Cliente] = []
+
+
+    def registrar_producto(self, producto: Producto) -> bool:
+
+        for elemento in self.productos:
+            if elemento.codigo == producto.codigo:
+                return False
+
+        self.productos.append(producto)
+        return True
+
+
+    def listar_productos(self) -> None:
+
+        for producto in self.productos:
+            print(producto.mostrar_informacion())
+
+
+    def registrar_cliente(self, cliente: Cliente) -> bool:
+
+        for elemento in self.clientes:
+            if elemento.identificacion == cliente.identificacion:
+                return False
+
+        self.clientes.append(cliente)
+        return True
+
+
+    def listar_clientes(self) -> None:
+
+        for cliente in self.clientes:
+            print(cliente.mostrar_informacion())
