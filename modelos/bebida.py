075517e3bfaf63056660892bd5810dc65c758306
nathalybohorquez16-glitch <nathalybohorquez16@gmail.com>
2026-07-17 02:55:49 -0500

    Add Bebida class inheriting from Producto

--- modelos/bebida.py
+++ modelos/bebida.py
@@ -1,1 +1,26 @@
+from modelos.producto import Producto
 
+
+class Bebida(Producto):
+
+    def __init__(
+        self,
+        codigo: str,
+        nombre: str,
+        categoria: str,
+        precio: float,
+        tamaño: str,
+        envase: str
+    ):
+        super().__init__(codigo, nombre, categoria, precio)
+        self.tamaño = tamaño
+        self.envase = envase
+
+    def mostrar_informacion(self) -> str:
+        return (
+            f"Código: {self.codigo} | "
+            f"Bebida: {self.nombre} | "
+            f"Tamaño: {self.tamaño} | "
+            f"Envase: {self.envase} | "
+            f"Precio: ${self.precio}"
+        )
